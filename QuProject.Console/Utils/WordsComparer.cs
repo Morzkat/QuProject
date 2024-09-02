a5791ee07c5dd5c718bdf07a5d97471bb3dd0f37
Morzkat <felix.david21@outlook.com>
2024-09-02 11:24:10 -0400

    * Initial commit.
    * Add logic for find words vertically and horizontally.
    
    *  Add method Find for find words and return the top 10 words.

---
+++ QuProject.Console/Utils/WordsComparer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuProject.Console.Utils
+{
+    public class WordsComparer : IEqualityComparer<string>
+    {
+        public bool Equals(string x, string y) => x.Contains(y);
+
+        public int GetHashCode(string obj) => obj.GetHashCode();
+    }
+}
---
+++ QuProject.Console/WordFinder.cs
@@ -0,0 +1,73 @@
+﻿using QuProject.Console.Utils;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace QuProject.Console
+{
+    public class WordFinder
+    {
+        private readonly int _cols;
+        private readonly int _rows;
+        private IList<string> _matrix;
+        private readonly HashSet<string> _matrixSet = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
+
+        public WordFinder(IEnumerable<string> matrix)
+        {
+            if (matrix == null || !matrix.Any()) throw new ArgumentException("Matrix cannot be null or empty.");
+
+            _matrix = matrix.ToList();
+            _rows = matrix.Count();
+            _cols = matrix.First().Count();
+
+            AddHorizontalWordsToMatrix();
+            AddVerticallyWordsToMatrix();
+        }
+
+        public IEnumerable<string> Find(IEnumerable<string> wordstream)
+        {
+            var foundWords = new Dictionary<string, int>();
+
+            foreach (var word in wordstream.Distinct())
+            {
+                if (_matrixSet.Contains(word, new WordsComparer()))
+                {
+                    if (!foundWords.ContainsKey(word))
+                        foundWords[word] = 1;
+                    else foundWords[word]++;
+                }
+            }
+
+            var topWords = foundWords
+                    .OrderByDescending(record => record.Value)
+                    .Take(10)
+                    .Select(record => record.Key);
+
+            return topWords;
+        }
+
+        private void AddHorizontalWordsToMatrix()
+        {
+            foreach (var row in _matrix) _matrixSet.Add(row);
+        }
+
+        public void AddVerticallyWordsToMatrix()
+        {
+            var matrixList = _matrix.ToList();
+            for (int i = 0; i < _cols; i++)
+            {
+                var word = "";
+                for (int j = 0; j < _rows; j++) word += matrixList[j][i];
+
+                _matrixSet.Add(word);
+            }
+        }
+
+    }
+}
+
+
