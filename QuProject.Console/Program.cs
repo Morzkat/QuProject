d4186da7ee830386009311eb1804a9aaf3b10f8d
Morzkat <felix.david21@outlook.com>
2024-09-02 11:31:29 -0400

    * Add test for validate Find words logic.

---
+++ QuProject.Console/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using QuProject.Console;
+
+// Example word stream
+List<string> matrix = new List<string> { "abcdc", "fgwio", "chill", "pqnsd", "uvdxy" };
+List<string> wordStream = new List<string> { "chill", "cold", "wind" };
+
+var finder = new WordFinder(matrix);
+var topWords = finder.Find(wordStream);
+Console.WriteLine($"Top words: {string.Join(",", topWords)}");
+
